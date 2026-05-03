9c9933ef3ced55ce60d22791b0ae220696c5f17c
benchchung <bchung28@calpoly.edu>
2026-05-03 15:00:07 -0700

    commit test

--- proj2.py
+++ proj2.py
@@ -5,7 +5,14 @@ from typing import *
 
 
 # Put your data definitions first!
+@dataclass (frozen=True)
+class Row:
+    line:str
 
+@dataclass (frozen=True)
+class Node:
+    value:Row
+    next: Node|None = None
 # ...
 
 # Then your functions.
