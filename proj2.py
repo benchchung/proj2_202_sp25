1aa400cce731ae5599e155bff47bc9ddc942d72f
benchchung <bchung28@calpoly.edu>
2026-05-03 15:13:47 -0700

    task 1 and begin function 1

--- proj2.py
+++ proj2.py
@@ -7,15 +7,23 @@ from typing import *
 # Put your data definitions first!
 @dataclass (frozen=True)
 class Row:
-    line:str
+    country:str
+    year:int
+    electricity_and_heat_co2_emissions:float|None
+    electricity_and_heat_co2_emissions_per_capita:float|None
+    energy_co2_emissions:float|None
+    energy_co2_emissions_per_capita:float|None
+    total_co2_emissions_excluding_lucf:float|None
+    total_co2_emissions_excluding_lucf_per_capita:float|None
 
 @dataclass (frozen=True)
 class Node:
     value:Row
     next: Node|None = None
-    #
+
 # ...
 
 # Then your functions.
-
+def read_csv_lines(filename: str) -> Optional[Node]:
+    pass
 # ...
