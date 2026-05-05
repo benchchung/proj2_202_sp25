f5428f7e73167f1b44da0d49714bf8ae7c6a6893
benchchung <bchung28@calpoly.edu>
2026-05-04 17:11:29 -0700

    added stuff to test file and began tests

--- test_student.py
+++ test_student.py
@@ -1,5 +1,14 @@
 import unittest
 from proj2 import *
 
+class TestsForEachFunction(unittest.TestCase):
+    def setUp(self):
+        self.data = read_csv_lines("ben_test_data.csv")
+
+    def test_listlen_empty(self):
+        self.assertEqual(listlen(None), 0)
+
+
+
 if __name__ == "__main__":
     unittest.main()
